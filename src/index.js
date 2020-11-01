a119215041ba7d7ae8ffb2408f3ed5c7c7ff8b7d
MartaKirchgessner <martakirchgessner@gmail.com>
2020-11-01 11:56:56 +0100

    Navigace pohyb na strance

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll ('nav a')
+console.log(navLinks);
+
+navBtn.addEventListener('click' , () => {
+  nav.classList.toggle('nav-closed');
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed')
+  })
+});
+
