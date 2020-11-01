cb4866ddc33928c68a7ced7f96c83e4b84edc3dd
MartaKirchgessner <martakirchgessner@gmail.com>
2020-11-01 12:33:04 +0100

    Objednavani

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './style.css';
 
 console.log('funguju!');
 
+/*Zprovoznění navigace*/
+
 const navBtn = document.querySelector('#nav-btn');
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll ('nav a')
@@ -17,4 +19,23 @@ navLinks.forEach((navLink) => {
     nav.classList.toggle('nav-closed')
   })
 });
+
+/*Objednávání nápojů*/
+const drinkCup = document.querySelector('.drink__cup');
+const orderBtn = document.querySelector('.order-btn');
+let ordered = false
+
+orderBtn.addEventListener('click', () => {
+  drinkCup.classList.toggle('drink__cup--selected');
+  if (ordered === false) {
+    orderBtn.textContent = 'Zrušit';
+  } else {
+    orderBtn.textContent = 'Objednat'
+  }
+  ordered = !ordered;
+});
+
+
+
+
 
